1befe964796402066570f0554eb10ca68d0ec553
Rafi Ferdos <rafiferdos@gmail.com>
2024-05-10 22:38:42 +0600

    feat: Add endpoint to retrieve all queries data
    The code changes add a new endpoint `/queries` to retrieve all queries data from the MongoDB database. This will allow clients to fetch and display the queries information.

--- index.js
+++ index.js
@@ -29,9 +29,15 @@ const client = new MongoClient(uri, {
 });
 async function run() {
     try {
-        // Connect the client to the server	(optional starting in v4.7)
-        // await client.connect();
-        // Send a ping to confirm a successful connection
+        const queriesCollection = client.db("swap_savvy_database").collection("queries")
+
+        //get all queries data
+        app.get('/queries', async (req, res) => {
+            const cursor = queriesCollection.find({})
+            const queries = await cursor.toArray()
+            res.send(queries)
+        })
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
