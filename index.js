58e54722e6a10d69767bf33234930ce67994a59c
Rafi Ferdos <rafiferdos@gmail.com>
2024-05-11 21:01:39 +0600

    add single data receiving gateway

--- index.js
+++ index.js
@@ -30,12 +30,20 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         const queriesCollection = client.db("swap_savvy_database").collection("queries")
+        const recommendationsCollection = client.db("swap_savvy_database").collection("recommendations")
 
         //get all queries data
         app.get('/queries', async (req, res) => {
             const cursor = queriesCollection.find({})
             const queries = await cursor.toArray()
             res.send(queries)
+        })
+
+        //get query by id
+        app.get('/queries/:id', async (req, res) => {
+            const id = req.params.id
+            const query = await queriesCollection.findOne({ _id: new ObjectId(id) })
+            res.send(query)
         })
 
         await client.db("admin").command({ ping: 1 });
