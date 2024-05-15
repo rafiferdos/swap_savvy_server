daf4766e5d3edc241c61612c7d04e6d7f21e2b2a
Rafi Ferdos <rafiferdos@gmail.com>
2024-05-15 09:59:31 +0600

    add deploy config

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 9000
 const app = express()
 
 const corsOptions = {
-    origin: ['http://localhost:5173', 'http://localhost:5174'],
+    origin: ['http://localhost:5173', 'http://localhost:5174', 'https://swapsavvy-4bd63.web.app'],
     credentials: true,
     optionsSuccessStatus: 200
 }
